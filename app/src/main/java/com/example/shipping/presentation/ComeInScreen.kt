cc11bbc4981956dd52f5403bed580c387f205f09
ARSEN TAROYAN <arsen2003taroyan@gmail.com>
2024-04-27 03:41:34 +0400

    Come in page

--- app/src/main/java/com/example/shipping/presentation/ComeInScreen.kt
+++ app/src/main/java/com/example/shipping/presentation/ComeInScreen.kt
@@ -1,12 +1,44 @@
 package com.example.shipping.presentation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -16,15 +48,63 @@ fun ComeInScreen(
     navController: NavHostController
 ) {
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
     ) {
-        Text(text = "ComeIn")
+        ComeInCard()
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun ComeInScreenPreview() {
+private fun ComeInScreenPreview() {
     ComeInScreen(navController = rememberNavController())
+}
+
+@Composable
+private fun ComeInCard() {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxHeight(0.30f)
+            .fillMaxWidth(0.80f),
+        shape = RoundedCornerShape(10.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "Come in", fontSize = 20.sp, fontWeight = FontWeight.Bold
+            )
+            EdText()
+            ComeInButton()
+        }
+    }
+}
+
+@Composable
+private fun ComeInButton() {
+    ElevatedButton(
+        onClick = {},
+        shape = RoundedCornerShape(10.dp),
+        elevation = ButtonDefaults.buttonElevation(defaultElevation = 4.dp)
+    ) {
+        Text(text = "Come in", color = Color.Black, fontSize = 18.sp)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun EdText() {
+    var textState by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = textState,
+        onValueChange = {
+            if(it.length <= 5) textState = it
+        },
+        maxLines = 1,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
 }
